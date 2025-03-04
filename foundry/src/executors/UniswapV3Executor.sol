c9da761929af0c213479be9c36aa93202d754f81
royvardhan <royvardhan.official@gmail.com>
2025-03-04 19:10:55 +0530

    fix: remove POOL_INIT_CODE_HASH from USV3 executor

--- foundry/src/executors/UniswapV3Executor.sol
+++ foundry/src/executors/UniswapV3Executor.sol
@@ -13,8 +13,6 @@ error UniswapV3Executor__InvalidFactory();
 contract UniswapV3Executor is IExecutor, ICallback {
     using SafeERC20 for IERC20;
 
-    bytes32 internal constant POOL_INIT_CODE_HASH =
-        0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
     uint160 private constant MIN_SQRT_RATIO = 4295128739;
     uint160 private constant MAX_SQRT_RATIO =
         1461446703485210103287273052203988822378723970342;
