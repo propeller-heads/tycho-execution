a6b884881b9aceb50049a2934b24a18b24a97aa9
Diana Carvalho <diana@propellerheads.xyz>
2025-04-10 10:15:24 +0100

    fix: Fix after merge with main
    Mostly had to delete some tests (because they were moved) and fix others
    
    Took 17 minutes
    
    Took 8 seconds
    
    Took 24 seconds

--- src/encoding/evm/strategy_encoder/strategy_encoders.rs
+++ src/encoding/evm/strategy_encoder/strategy_encoders.rs
@@ -2193,7 +2193,7 @@ mod tests {
             eth_chain(),
             swap_encoder_registry,
             None,
-            Some(Bytes::from_str("0x3Ede3eCa2a72B3aeCC820E955B36f38437D01395").unwrap()),
+            Bytes::from_str("0x3Ede3eCa2a72B3aeCC820E955B36f38437D01395").unwrap(),
         )
         .unwrap();
 
@@ -2255,7 +2255,7 @@ mod tests {
             eth_chain(),
             swap_encoder_registry,
             None,
-            Some(Bytes::from_str("0x3Ede3eCa2a72B3aeCC820E955B36f38437D01395").unwrap()),
+            Bytes::from_str("0x3Ede3eCa2a72B3aeCC820E955B36f38437D01395").unwrap(),
         )
         .unwrap();
 
