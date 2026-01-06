09c77854ca86c269da95601cc2efeb38865bbe72
TAMARA LIPOWSKI <data.lipowski@extaccount.com>
2026-01-05 22:24:26 -0500

    fix: Deploy FeeExecutor in TychoRouterTestSetup

--- foundry/test/TychoRouterTestSetup.sol
+++ foundry/test/TychoRouterTestSetup.sol
@@ -20,6 +20,7 @@ import {SlipstreamsExecutor} from "../src/executors/SlipstreamsExecutor.sol";
 import {RocketpoolExecutor} from "../src/executors/RocketpoolExecutor.sol";
 import {ERC4626Executor} from "../src/executors/ERC4626Executor.sol";
 import {LidoExecutor} from "../src/executors/LidoExecutor.sol";
+import {FeeExecutor} from "../src/executors/FeeExecutor.sol";
 
 // Test utilities and mocks
 import "./Constants.sol";
@@ -101,6 +102,7 @@ contract TychoRouterTestSetup is Constants, Permit2TestHelper, TestUtils {
 
     ERC4626Executor public erc4626Executor;
     LidoExecutor public lidoExecutor;
+    FeeExecutor public feeExecutor;
 
     function getChain() public view virtual returns (string memory) {
         return "mainnet";
@@ -123,6 +125,7 @@ contract TychoRouterTestSetup is Constants, Permit2TestHelper, TestUtils {
         address[] memory executors = deployExecutors();
         vm.startPrank(EXECUTOR_SETTER);
         tychoRouter.setExecutors(executors);
+        tychoRouter.setFeeExecutor(address(feeExecutor));
         vm.stopPrank();
     }
 
@@ -183,8 +186,9 @@ contract TychoRouterTestSetup is Constants, Permit2TestHelper, TestUtils {
         erc4626Executor = new ERC4626Executor(PERMIT2_ADDRESS);
         lidoExecutor =
             new LidoExecutor(STETH_ADDR, WSTETH_ADDR, PERMIT2_ADDRESS);
+        feeExecutor = new FeeExecutor(PERMIT2_ADDRESS);
 
-        address[] memory executors = new address[](16);
+        address[] memory executors = new address[](17);
         executors[0] = address(usv2Executor);
         executors[1] = address(usv3Executor);
         executors[2] = address(pancakev3Executor);
@@ -201,6 +205,7 @@ contract TychoRouterTestSetup is Constants, Permit2TestHelper, TestUtils {
         executors[13] = address(rocketpoolExecutor);
         executors[14] = address(erc4626Executor);
         executors[15] = address(lidoExecutor);
+        executors[16] = address(feeExecutor);
 
         return executors;
     }
